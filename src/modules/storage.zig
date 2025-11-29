5a602cf769b6513035d130aa5b4e57c1be24ade3
Alvaro <89421445+alvaro17f@users.noreply.github.com>
2025-11-29 23:22:38 +0100

    fix: storage module

--- src/modules/storage.zig
+++ src/modules/storage.zig
@@ -9,22 +9,25 @@ pub const DiskInfo = struct {
     disk_usage_percentage: u8,
 };
 
+const GB = 1024 * 1024 * 1024;
+
 pub fn getStorage(allocator: std.mem.Allocator, disk_path: []const u8) ![]const u8 {
     var stat: c_statvfs.struct_statvfs = undefined;
     if (c_statvfs.statvfs(disk_path.ptr, &stat) != 0) {
         return error.StatvfsFailed;
     }
 
-    const total_size = stat.f_blocks * stat.f_frsize;
-    const free_size = stat.f_bfree * stat.f_frsize;
-    const used_size = total_size - free_size;
-
+    const block_size = stat.f_bsize;
+    const total_blocks = stat.f_blocks;
+    const available_blocks = stat.f_bavail;
+    const total_size = total_blocks * block_size;
+    const used_size = total_size - (available_blocks * block_size);
     const used_size_percentage = (used_size * 100) / total_size;
 
     const disk_info = DiskInfo{
         .disk_path = disk_path,
-        .disk_size = @as(f64, @floatFromInt(total_size)) / 1e9,
-        .disk_usage = @as(f64, @floatFromInt(used_size)) / 1e9,
+        .disk_size = @as(f64, @floatFromInt(total_size)) / GB,
+        .disk_usage = @as(f64, @floatFromInt(used_size)) / GB,
         .disk_usage_percentage = @as(u8, @intCast(used_size_percentage)),
     };
 
